5c5019fcba6d4ae2fa383e969e7bc5fba1baac72
IELA <andreitumbaga29@gmail.com>
2023-01-17 20:43:30 +0800

    Add project files.

---
+++ Tumbaga_midterm/Controllers/TrigoController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tumbaga_midterm.Controllers
+{
+    [Route("Controller")]
+    [ApiController]
+    public class TrigController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<TrigResult> Post([FromBody] TrigInput input)
+        {
+            var sin = Math.Sin(input.Angle);
+            var cos = Math.Cos(input.Angle);
+            var tan = Math.Tan(input.Angle);
+            var result = new TrigResult { Sin = sin, Cos = cos, Tan = tan };
+            return result;
+        }
+    }
+}
---
+++ Tumbaga_midterm/TrigoInputResult.cs
@@ -0,0 +1,14 @@
+namespace Tumbaga_midterm
+{
+    public class TrigInput
+    {
+        public double Angle { get; set; }
+    }
+
+    public class TrigResult
+    {
+        public double Sin { get; set; }
+        public double Cos { get; set; }
+        public double Tan { get; set; }
+    }
+}
